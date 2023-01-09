74160218418df4f14c4b2f5a9dc636277662fb19
Archit404Error <4architmehta@gmail.com>
2023-01-08 20:07:26 -0500

    Implement roster links

--- frontend/src/courseQueries/index.js
+++ frontend/src/courseQueries/index.js
@@ -15,12 +15,18 @@ const CourseQueries = () => {
         <div className="CourseQueries">
             <SearchBar submitFunc={fetchSimiliarCourses} />
             {
+                courses.length > 0 && courses[0].similarity < 0.4 &&
+                <div style={{ width: "60vw", textAlign: 'left', marginTop: 30, backgroundColor: '#3E4451', padding: 40, paddingLeft: 60 }}>
+                    <p>Note: there were no great matches on this topic, but some potential matches are displayed below.</p>
+                </div>
+            }
+            {
                 courses.map((course, i) => (
                     <div key={`courseRec${i}`} style={{ width: "60vw", textAlign: 'left', marginTop: 30, backgroundColor: '#3E4451', padding: 40, paddingLeft: 60 }}>
                         <h3 className="smallMargin">{course.title} ({course.dept} {course.number})</h3>
-                        <p className="smallMargin">{Math.round(course.similarity * 10000) / 100}% query similarity score</p>
+                        <span><p className="smallMargin">{Math.round(course.similarity * 10000) / 100}% query similarity score</p></span>
                         <p className="smallMargin">{course.desc}</p>
-                        <a target="_blank" href="https://google.com" rel="noreferrer">View on Class Roster</a>
+                        <a target="_blank" href={`https://classes.cornell.edu/browse/roster/SP23/class/${course.dept}/${course.number}`} rel="noreferrer">View on Class Roster</a>
                     </div>
                 ))
             }
