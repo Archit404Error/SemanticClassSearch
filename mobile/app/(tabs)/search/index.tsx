fca6a194de0b5e4e7e42660c02d40faca027121f
Archit404Error <4architmehta@gmail.com>
2023-04-02 12:39:06 -0400

    Implement basic styling

--- mobile/app/(tabs)/search/index.tsx
+++ mobile/app/(tabs)/search/index.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
-import { SafeAreaView, TextInput } from "react-native";
+import { SafeAreaView, StyleSheet, TextInput, View, TouchableOpacity } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
+import { DoneWrapper, doneWrapperId } from "../../../components/utils/doneWrapper";
 import { SERVER_URL } from "../../../constants";
+import { Ionicons } from "@expo/vector-icons"
 import CourseCard from "../../../components/courseSearch/courseCard";
 
 const courseRecs = async (query?: string): Promise<CourseRec[]> => {
@@ -16,20 +18,63 @@ const Search = () => {
     const [recs, setRecs] = useState<CourseRec[]>();
 
     return (
-        <SafeAreaView>
+        <SafeAreaView style={styles.container}>
             <ScrollView>
-                <TextInput
-                    onChangeText={setQuery}
-                    onSubmitEditing={async () => setRecs(await courseRecs(query))}
-                    placeholder="Testing"
-                    style={{ fontSize: 20, backgroundColor: "red", color: "black" }}
-                />
+                <View style={styles.parent}>
+                    <TextInput
+                        value={query}
+                        onChangeText={setQuery}
+                        onSubmitEditing={async () => setRecs(await courseRecs(query))}
+                        placeholder="Search for classes..."
+                        style={styles.searchBar}
+                        inputAccessoryViewID={doneWrapperId}
+                    />
+                    <TouchableOpacity
+                        style={styles.closeButtonParent}
+                        onPress={() => setQuery("")}
+                    >
+                        <Ionicons name="close-circle-outline" size={30} />
+                    </TouchableOpacity>
+                </View>
                 {
-                    recs?.map(rec => <CourseCard {...rec} />)
+                    recs?.map((rec, index) => <CourseCard key={`rec${index}`} {...rec} />)
                 }
             </ScrollView>
+            <DoneWrapper />
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "center",
+    },
+    parent: {
+        marginLeft: 25,
+        marginRight: 25,
+        borderColor: "gray",
+        backgroundColor: "white",
+        borderRadius: 5,
+        borderWidth: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+    closeButton: {
+        height: 16,
+        width: 16,
+    },
+    closeButtonParent: {
+        justifyContent: "center",
+        alignItems: "center",
+        marginRight: 5,
+    },
+    searchBar: {
+        padding: 10,
+        fontSize: 20,
+        color: "black"
+    },
+})
 
 export default Search;
--- mobile/components/courseSearch/courseCard.tsx
+++ mobile/components/courseSearch/courseCard.tsx
@@ -1,14 +1,38 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const CourseCard = ({ title, dept, number, similarity, desc }: CourseRec) => {
     return (
-        <View>
-            <Text>{title}</Text>
-            <Text>({dept} {number})</Text>
-            <Text>{similarity}</Text>
-            <Text>{desc}</Text>
+        <View style={styles.cardContainer}>
+            <Text style={styles.cardTitle}>{title} ({dept} {number})</Text>
+            <Text style={styles.aiHeader}>
+                {(similarity * 100).toFixed(2)}% Query Similarity
+            </Text>
+            <Text style={styles.cardBody}>{desc}</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    cardContainer: {
+        backgroundColor: "#ffffff",
+        marginTop: 5,
+        marginBottom: 5,
+        borderRadius: 5,
+        padding: 10,
+    },
+    cardTitle: {
+        fontSize: 20,
+        fontWeight: "bold",
+        marginBottom: 5,
+    },
+    aiHeader: {
+        fontSize: 18,
+        color: "#65a7fc",
+        marginBottom: 5,
+    },
+    cardBody: {
+        fontSize: 15,
+    },
+})
 
 export default CourseCard;
