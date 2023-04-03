3c3759ad3831d5c1c2f2205e5be9931ab26fbab9
Archit404Error <4architmehta@gmail.com>
2023-04-02 23:51:25 -0400

    Implement course details page

--- mobile/app/(tabs)/search/[course].tsx
+++ mobile/app/(tabs)/search/[course].tsx
@@ -1,59 +1,65 @@
 import { useState, useEffect } from "react";
 import { useLocalSearchParams } from "expo-router";
-import { Text } from "react-native";
-import { ROSTER_API_4 } from "../../../constants";
-
-interface Instructor {
-    netid: string,
-    lastname: string,
-    firstname: string,
-}
+import { SafeAreaView, ScrollView, View, Text, StyleSheet } from "react-native";
+import { ROSTER_API_4, SERVER_URL } from "../../../constants";
 
 interface CourseInfo {
-    instructors: Instructor[],
-    titleShort: string,
-    classNbr: number,
-    acadGroup: string,
-    locationDescr: string,
-}
-
-const Instructors = ({ instructors }: CourseInfo) => {
-    return (
-        <>
-            {
-                instructors.map(instructor =>
-                    <Text>
-                        {instructor.firstname} {instructor.lastname} ({instructor.netid})
-                    </Text>
-                )
-            }
-        </>
-    )
+    catalogComments?: string,
+    catalogDistr?: string,
+    catalogForbiddenOverlaps?: string,
+    catalogOutcomes?: string[],
+    catalogPrereqCoreq?: string,
+    catalogWhenOffered?: string,
+    description?: string,
+    titleShort?: string,
 }
 
 const CourseDetails = () => {
-    const [courseInfo, setCourseInfo] = useState<CourseInfo[]>()
+    const [courseInfo, setCourseInfo] = useState<CourseInfo>()
     const { course, num } = useLocalSearchParams();
 
     useEffect(() => {
-        fetch(`${ROSTER_API_4}/class-syllabi?rosterSlug=SP23&subject=${course}&catalogNbr=${num}`)
+        fetch(`${SERVER_URL}/course-info?course=${course}+${num}`)
             .then(res => res.json())
             .then(json => setCourseInfo(json))
     }, [])
 
     return (
-        <>
-            <Text>{course} {num}</Text>
-            {courseInfo?.map((lec, ind) => (
-                <>
-                    <Text>{lec.titleShort}</Text>
-                    <Instructors {...lec} />
-                </>
-            ))
-
-            }
-        </>
+        <SafeAreaView style={styles.container}>
+            <ScrollView style={styles.subcontainer}>
+                <Text style={styles.title}>{courseInfo?.titleShort} ({course} {num})</Text>
+                <Text style={styles.subheader}>Offered: {courseInfo?.catalogWhenOffered}</Text>
+                <Text style={styles.subheader}>Distribution: {courseInfo?.catalogDistr}</Text>
+                <Text style={styles.subheader}>Forbidden Overlaps: {courseInfo?.catalogForbiddenOverlaps}</Text>
+                <Text style={styles.subheader}>Description: {courseInfo?.description}</Text>
+                <Text style={styles.subheader}>Outcomes:</Text>
+                {
+                    courseInfo?.catalogOutcomes?.map((outcome, idx) =>
+                        <Text key={`outcome${idx}`} style={styles.subheader}>- {outcome}</Text>)
+                }
+                <Text style={styles.subheader}>Requirements: {courseInfo?.catalogPrereqCoreq}</Text>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "white",
+    },
+    subcontainer: {
+        padding: 10,
+    },
+    title: {
+        fontSize: 25,
+        fontWeight: "bold",
+        marginBottom: 10,
+    },
+    subheader: {
+        fontSize: 18,
+        marginBottom: 10,
+    },
+})
 
 export default CourseDetails;
